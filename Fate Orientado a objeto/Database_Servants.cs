0bc7c63ba70ed930981c685b1a5316017ced2dad
victor eduardo teixeira lima <victor_lima72@hotmail.com>
2019-11-12 17:22:39 -0300

    List was made correctly

--- Fate Orientado a objeto/Database_Servants.cs
+++ Fate Orientado a objeto/Database_Servants.cs
@@ -12,41 +12,6 @@ namespace Fate_Orientado_a_objeto
         public List<Movimentos> muve;
         public void Servants_creation()
         {
-            muve = new List<Movimentos>
-            {
-                new Movimentos()
-                {
-                    Name_move = "Buster",
-                    Special = false,
-                    Atk_bonus = 500,
-                    Def_bonus = 0,
-                    Charge_bonus= 0
-                },
-                new Movimentos()
-                {
-                    Name_move = "Arts",
-                    Special = false,
-                    Atk_bonus = 100,
-                    Def_bonus = 200,
-                    Charge_bonus = 25
-                },
-                new Movimentos()
-                {
-                    Name_move = "Quick",
-                    Special = false,
-                    Atk_bonus = 200,
-                    Def_bonus = 100,
-                    Charge_bonus = 10
-                },
-                new Movimentos()
-                {
-                    Name_move = "Noble Phatasma",
-                    Special = true,
-                    Atk_bonus = 3000,
-                    Def_bonus = 0,
-                    Charge_bonus = -100
-                }
-            };
             servos = new List<Servants>
             {
                 new Servants()
@@ -56,16 +21,44 @@ namespace Fate_Orientado_a_objeto
                     Charge_spe = 0,
                     Atk = 1500,
                     Def = 800,
-                    Health = 5000
+                    Health = 5000,
+                    Moves = new List<Movimentos>()
+                    {
+                        new Movimentos()
+                        {
+                            Name_move = "Buster",
+                            Special = false,
+                            Atk_bonus = 500,
+                            Def_bonus = 0,
+                            Charge_bonus= 0
+                        },
+                        new Movimentos()
+                        {
+                            Name_move = "Arts",
+                            Special = false,
+                            Atk_bonus = 100,
+                            Def_bonus = 200,
+                            Charge_bonus = 25
+                        },
+                        new Movimentos()
+                        {
+                            Name_move = "Quick",
+                            Special = false,
+                            Atk_bonus = 200,
+                            Def_bonus = 100,
+                            Charge_bonus = 10
+                        },
+                        new Movimentos()
+                        {
+                            Name_move = "Noble Phatasma",
+                            Special = true,
+                            Atk_bonus = 3000,
+                            Def_bonus = 0,
+                            Charge_bonus = -100
+                        }
+                    }
                 }
             };
-            foreach(var serv in servos)
-            {
-                foreach(var mu in muve)
-                {
-                    serv.Moves.Add(mu);
-                }
-            }
         }
     }
 }
--- Fate Orientado a objeto/Movimentos.cs
+++ Fate Orientado a objeto/Movimentos.cs
@@ -14,6 +14,16 @@ namespace Fate_Orientado_a_objeto
         private float def_bonus;
         private float charge_bonus;
 
+        public Movimentos() { }
+        public Movimentos(string name_move, bool special, float atk_bonus, float def_bonus, float charge_bonus)
+        {
+            this.name_move = name_move;
+            this.special = special;
+            this.atk_bonus = atk_bonus;
+            this.def_bonus = def_bonus;
+            this.charge_bonus = charge_bonus;
+        }
+
         public string Name_move
         {
             get => name_move;
