4dbc7bfabb4e6e605ed5892379a59286b4225adc
victor eduardo teixeira lima <victor_lima72@hotmail.com>
2019-11-20 22:10:23 -0300

    S2 Tiko

--- Fate Orientado a objeto/Program.cs
+++ Fate Orientado a objeto/Program.cs
@@ -18,6 +18,11 @@ namespace Fate_Orientado_a_objeto
             Player2.Master_Creation(2);
             Console.WriteLine("Hora de escolher um servo para começar a batalha, começaremos pelo jogador 1, {0}", Player.Name);
             Console.WriteLine("Escolha um por favor");
+            Player.Servants_mas = new List<Servants>();
+            /*Player.Servants_mas.Add(data.List_of_servants)*/;
+            data.List_of_servants();
+            Console.WriteLine("Hora de escolher um servo para começar a batalha, começaremos pelo jogador 2, {0}", Player2.Name);
+            Console.WriteLine("Escolha um por favor");
             data.List_of_servants();
 
             Console.ReadLine();
